1a7d8c01134b7b05ce78161ed04cb74ff7ab98ee
Carl Meyer <carl@dirtcircle.com>
2010-03-19 02:47:36 -0400

    bump version for dev

--- metadata.py
+++ metadata.py
@@ -1,5 +1,26 @@
+import subprocess
+import os.path
+
+try:
+    # don't get confused if our sdist is unzipped in a subdir of some 
+    # other hg repo
+    if os.path.isdir('.hg'):
+        p = subprocess.Popen(['hg', 'parents', r'--template={rev}\n'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not p.returncode:
+            fh = open('HGREV', 'w')
+            fh.write(p.communicate()[0].splitlines()[0])
+            fh.close()
+except (OSError, IndexError):
+    pass
+    
+try:
+    hgrev = open('HGREV').read()
+except IOError:
+    hgrev = ''
+
 name = 'localeurl'
 authors = 'Joost Cassee, Artiom Diomin and Carl Meyer'
 copyright_years = '2008-2010'
-version = '1.4'
+version = '1.4.post%s' % hgrev
 release = version
