c9b30f4ce088b5aa74d65889e36e000e78310bc1
Carl Meyer <carl@oddbird.net>
2011-04-15 13:51:01 -0500

    Added tox.ini and fixed test-runner for compatibility with Django 1.0-trunk.

--- runtests.py
+++ runtests.py
@@ -3,23 +3,41 @@
 from os.path import dirname, abspath
 import sys
 
-from django.conf import settings as django_settings
+from django.conf import settings
 
-if not django_settings.configured:
-    django_settings.configure(
-        DATABASE_ENGINE='sqlite3',
+if not settings.configured:
+    from django import VERSION
+    settings_dict = dict(
         INSTALLED_APPS=(
             'localeurl',
-        ),
+            ),
         ROOT_URLCONF='localeurl.tests.test_urls',
-    )
+        )
+    if VERSION >= (1, 2):
+        settings_dict["DATABASES"] = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3"
+                }}
+    else:
+        settings_dict["DATABASE_ENGINE"] = "sqlite3"
+
+    settings.configure(**settings_dict)
+
 
 def runtests(*test_args):
     if not test_args:
         test_args = ['localeurl']
     parent = dirname(abspath(__file__))
     sys.path.insert(0, parent)
-    from django.test.simple import run_tests
+    try:
+        from django.test.simple import DjangoTestSuiteRunner
+        def run_tests(test_args, verbosity, interactive):
+            runner = DjangoTestSuiteRunner(
+                verbosity=verbosity, interactive=interactive)
+            return runner.run_tests(test_args)
+    except ImportError:
+        # for Django versions that don't have DjangoTestSuiteRunner
+        from django.test.simple import run_tests
     failures = run_tests(test_args, verbosity=1, interactive=True)
     sys.exit(failures)
 
